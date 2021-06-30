49cbe8f1fcf5aefd73a8301b15431644a9c59213
Amin Tai <amintai157@gmail.com>
2021-06-30 12:29:04 +0530

    first commit

---
+++ src/containers/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div>
+            <div>
+                <h1>FakeShop</h1>
+            </div>
+        </div>
+    )
+}
+export default Header
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,17 @@
+import { ActionTypes } from "../constants/action-type";
+
+const initialState = {
+    products:[{
+        id: 1,
+        title: "Amin",
+        category: "Programmer"
+    }]
+}
+export const productReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+        default:
+            return state;
+    }
+}
